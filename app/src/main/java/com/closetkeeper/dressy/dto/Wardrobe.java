a810b594b41d95ee87ed219f7f61090ad144fc15
cdaniel <cjamesdaniel17@gmail.com>
2022-10-17 14:59:06 -0500

    Added and clarified comments in Wardrobe class.

--- app/src/main/java/com/closetkeeper/dressy/dto/Wardrobe.java
+++ app/src/main/java/com/closetkeeper/dressy/dto/Wardrobe.java
@@ -5,6 +5,8 @@ import java.util.*;
 /**
  * The Wardrobe is the way a user can view an inventory of all stored Items and Outfits they have
  * associated with their Account.
+ *
+ * Created by Curran
  */
 
 public class Wardrobe {
@@ -13,27 +15,32 @@ public class Wardrobe {
     private List<String> clothes;   //TODO: Rename type String to Items
     private List<String> outfits;   //TODO: Rename type String to Outfit
 
-
+    //Returns the user's Account ID
     public int getAccount_id() {
         return account_id;
     }
 
+    //Sets the Account ID as the int that is passed
     public void setAccount_id(int account_id) {
         this.account_id = account_id;
     }
 
+    //Returns the list of Items[] from clothes
     public List<String> getClothes() {
         return clothes;
     }
 
+    //Sets the List of clothes into List<Clothes> clothes
     public void setClothes(List<String> clothes) {
         this.clothes = clothes;
     }
 
+    //Returns the list of Outfits[] from outfits
     public List<String> getOutfits() {
         return outfits;
     }
 
+    //Sets the List of outfits into List<Outfits> outfits
     public void setOutfits(List<String> outfits) {
         this.outfits = outfits;
     }
