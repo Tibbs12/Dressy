b04a1398c06130bba6477b1f4e3123cf675d07b8
Ruuse <russom4883@uhcl.edu>
2022-10-29 20:34:25 -0500

    Much needed commit

--- app/src/main/java/com/closetkeeper/dressy/ui/MainActivity.java
+++ app/src/main/java/com/closetkeeper/dressy/ui/MainActivity.java
@@ -3,6 +3,7 @@ package com.closetkeeper.dressy.ui;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
 
 import com.closetkeeper.dressy.R;
 
@@ -12,5 +13,11 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+    }
+    public void SignIn(View v) {
+        setContentView(R.layout.activity_signin);
+    }
+    public void SignUp(View v) {
+        setContentView(R.layout.activity_signup);
     }
 }
