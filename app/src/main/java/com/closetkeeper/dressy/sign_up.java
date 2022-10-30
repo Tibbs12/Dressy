22c2e198e39741e3c87ad5f345f70b8fc373f6cb
Ruuse <russom4883@uhcl.edu>
2022-10-29 21:25:03 -0500

    one more

--- app/src/main/java/com/closetkeeper/dressy/sign_up.java
+++ app/src/main/java/com/closetkeeper/dressy/sign_up.java
@@ -2,13 +2,29 @@ package com.closetkeeper.dressy;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.TextView;
 
 public class sign_up extends AppCompatActivity {
-
+    private TextView textView6;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_signup);
+        textView6 = (TextView) findViewById(R.id.textView6);
+        textView6.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                openSignIn();
+            }
+        });
     }
+
+    public void openSignIn() {
+        Intent intent = new Intent(this, com.closetkeeper.dressy.sign_in.class);
+        startActivity(intent);
+    }
+
 }
