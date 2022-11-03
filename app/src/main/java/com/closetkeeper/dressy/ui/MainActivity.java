d7b0409655268d12b7cc7778fdc45b56a4d1f62c
Ruuse <russom4883@uhcl.edu>
2022-11-02 21:52:25 -0500

    added mainactivity.java file

--- app/src/main/java/com/closetkeeper/dressy/ui/MainActivity.java
+++ app/src/main/java/com/closetkeeper/dressy/ui/MainActivity.java
@@ -10,38 +10,18 @@ import android.widget.Button;
 import com.closetkeeper.dressy.R;
 
 public class MainActivity extends AppCompatActivity {
-    private Button signIn;
-    private Button signUp;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        signIn = (Button) findViewById(R.id.signIn);
-        signIn.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                openSignIn();
-            }
-        });
 
-        signUp = (Button) findViewById(R.id.signUp);
-        signUp.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                openSignUp();
-            }
-        });
+        //We will first display the contents of the splash screen and then move to access page
+
+        setContentView(R.layout.activity_access);
+
     }
 
-    public void openSignUp() {
-        Intent intent = new Intent(this, com.closetkeeper.dressy.sign_up.class);
-        startActivity(intent);
-    }
-
-    public void openSignIn() {
-        Intent intent = new Intent(this, com.closetkeeper.dressy.sign_in.class);
-        startActivity(intent);
-    }
 
 
 }
