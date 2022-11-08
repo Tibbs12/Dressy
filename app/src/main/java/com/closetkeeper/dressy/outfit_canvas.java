1832e54a3da1fcbd2abd7999e75f067109ccf1b8
smcghee97 <mcgheeshani@gmail.com>
2022-11-07 23:08:09 -0600

    outfit canvas layout

---
+++ app/src/main/java/com/closetkeeper/dressy/outfit_canvas.java
@@ -0,0 +1,14 @@
+package com.closetkeeper.dressy;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class outfit_canvas extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_outfit_canvas);
+    }
+}
