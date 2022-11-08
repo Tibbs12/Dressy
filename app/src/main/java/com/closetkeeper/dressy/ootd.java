debebf43e45c67711af603ba2776ae2122cc352b
smcghee97 <mcgheeshani@gmail.com>
2022-11-08 00:27:41 -0600

    ootd calendar
    displays ootd for a selected date etc

---
+++ app/src/main/java/com/closetkeeper/dressy/ootd.java
@@ -0,0 +1,14 @@
+package com.closetkeeper.dressy;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class ootd extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_ootd);
+    }
+}
