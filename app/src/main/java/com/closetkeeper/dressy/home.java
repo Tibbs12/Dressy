e928fdd475047fc3064aa3d90200847378886cfe
Ruuse <russom4883@uhcl.edu>
2022-11-13 18:18:22 -0600

    added nav bar functionality, still work to be done on it however

--- app/src/main/java/com/closetkeeper/dressy/home.java
+++ app/src/main/java/com/closetkeeper/dressy/home.java
@@ -2,29 +2,81 @@ package com.closetkeeper.dressy;
 
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.appcompat.widget.AppCompatImageButton;
+import androidx.core.app.ActivityCompat;
+import androidx.fragment.app.Fragment;
+import androidx.fragment.app.FragmentManager;
+import androidx.fragment.app.FragmentTransaction;
 
+import android.Manifest;
+import android.app.Activity;
+import android.app.ActivityManager;
 import android.content.Intent;
+import android.content.pm.PackageManager;
+import android.graphics.Bitmap;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
 import android.widget.ImageButton;
+import android.widget.ImageView;
 import android.widget.LinearLayout;
 import android.widget.TextView;
+import android.widget.Toast;
 
-import org.w3c.dom.Text;
+import com.closetkeeper.dressy.databinding.ActivityHomeBinding;
+
 
 public class home extends AppCompatActivity {
+
+    ActivityHomeBinding binding;
+
 
     private ImageButton newclosetbtn;           /**Instantiating the two ImageButtons*/
     private ImageButton newoutfitbtn;
     private TextView calendarClick;
     private LinearLayout MyClosets;
     private LinearLayout MyOutfits;
+    /** Used for permissions to access camera*/
+    public static final int RequestPermissionCode = 1;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_home);
+        binding = ActivityHomeBinding.inflate(getLayoutInflater());
+        setContentView(binding.getRoot());
+
+
+        //Navigation menu code
+        binding.bottomNavBar.setOnItemSelectedListener(item -> {
+
+            switch(item.getItemId()){
+                case R.id.homeNavBtn:
+                    Intent intent = new Intent(this, com.closetkeeper.dressy.home.class);
+                    startActivity(intent);
+                    break;
+                case R.id.closetNavBtn:
+                    Intent closet = new Intent(this, com.closetkeeper.dressy.my_closets.class);
+                    startActivity(closet);
+                    break;
+                case R.id.addNavBtn:
+                    /**Intent add = new Intent(this, com.closetkeeper.dressy.???.class);
+                     startActivity(add); */
+                    EnableRuntimePermission();
+                    Intent camera = new Intent(android.provider.MediaStore.ACTION_IMAGE_CAPTURE);
+                    startActivityForResult(camera, 7); /** this is connected to "OnActivityResult" Method */
+                    break;
+                case R.id.searchNavBar:
+                    /** Intent search = new Intent(this, com.closetkeeper.dressy.my_closets.class);
+                     startActivity(search); */
+                    break;
+                case R.id.outfitsNavBtn:
+                    Intent outfit = new Intent(this, com.closetkeeper.dressy.my_outfits.class);
+                    startActivity(outfit);
+                    break;
+            }
+
+
+            return true;
+        });
 
         MyClosets = (LinearLayout) findViewById(R.id.MyClosets);
         MyOutfits = (LinearLayout) findViewById(R.id.MyOutfits);
@@ -80,4 +132,41 @@ public class home extends AppCompatActivity {
         Intent intent = new Intent(this, com.closetkeeper.dressy.calendar.class);
         startActivity(intent);
     }
+
+    /** methods for camera */
+    private void EnableRuntimePermission() {
+        if (ActivityCompat.shouldShowRequestPermissionRationale(home.this, Manifest.permission.CAMERA)) {
+            Toast.makeText(home.this, "CAMERA permission allows us to Access your camera", Toast.LENGTH_LONG).show();
+        }
+        else
+        {
+            ActivityCompat.requestPermissions(home.this, new String[]{Manifest.permission.CAMERA}, RequestPermissionCode);
+        }
+    }
+
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+        if (requestCode == 7 && resultCode == RESULT_OK) {
+            Bitmap bitmap = (Bitmap) data.getExtras().get("data");
+            /** Instead of all below code, we will pass bitmap and tag string to method CreateItem(); */
+            //ImageView image = new ImageView(this);/** This code adds a button each time*/
+            //image.setLayoutParams(gridLayout.getLayoutParams());
+            //gridLayout.addView(image);
+            //image.setImageBitmap(bitmap);
+        }
+    }
+
+    public void onRequestPermissionsResult(int requestCode, String permissions[], int[] result) {
+        super.onRequestPermissionsResult(requestCode, permissions, result);
+        switch (requestCode) {
+            case RequestPermissionCode:
+                if (result.length > 0 && result[0] == PackageManager.PERMISSION_GRANTED) {
+                    Toast.makeText(home.this, "Permission Granted, Now your application can access CAMERA.", Toast.LENGTH_LONG).show();
+                } else {
+                    Toast.makeText(home.this, "Permission Canceled, Now your application cannot access CAMERA.", Toast.LENGTH_LONG).show();
+                }
+                break;
+        }
+    }
+
 }
--- app/src/main/java/com/closetkeeper/dressy/my_outfits.java
+++ app/src/main/java/com/closetkeeper/dressy/my_outfits.java
@@ -1,14 +1,99 @@
 package com.closetkeeper.dressy;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.app.ActivityCompat;
 
+import android.Manifest;
+import android.content.Intent;
+import android.content.pm.PackageManager;
+import android.graphics.Bitmap;
 import android.os.Bundle;
+import android.widget.Toast;
+
+import com.closetkeeper.dressy.databinding.ActivityMyClosetsBinding;
+import com.closetkeeper.dressy.databinding.ActivityMyOutfitsBinding;
 
 public class my_outfits extends AppCompatActivity {
+
+    ActivityMyOutfitsBinding binding;
+    public static final int RequestPermissionCode = 1;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_my_outfits);
+        binding = ActivityMyOutfitsBinding.inflate(getLayoutInflater());
+        setContentView(binding.getRoot());
+        binding.bottomNavBar.setSelectedItemId(R.id.outfitsNavBtn);  /** this sets the correct nav button each time we load this page */
+
+        //Navigation menu code
+        //Navigation menu code
+        binding.bottomNavBar.setOnItemSelectedListener(item -> {
+
+            switch(item.getItemId()){
+                case R.id.homeNavBtn:
+                    Intent intent = new Intent(this, com.closetkeeper.dressy.home.class);
+                    startActivity(intent);
+                    break;
+                case R.id.closetNavBtn:
+                    Intent closet = new Intent(this, com.closetkeeper.dressy.my_closets.class);
+                    startActivity(closet);
+                    break;
+                case R.id.addNavBtn:
+                    /**Intent add = new Intent(this, com.closetkeeper.dressy.???.class);
+                    startActivity(add); */
+                    EnableRuntimePermission();
+                    Intent camera = new Intent(android.provider.MediaStore.ACTION_IMAGE_CAPTURE);
+                    startActivityForResult(camera, 7); /** this is connected to "OnActivityResult" Method */
+                    break;
+                case R.id.searchNavBar:
+                    /** Intent search = new Intent(this, com.closetkeeper.dressy.my_closets.class);
+                     startActivity(search); */
+                    break;
+                case R.id.outfitsNavBtn:
+                    Intent outfit = new Intent(this, com.closetkeeper.dressy.my_outfits.class);
+                    startActivity(outfit);
+                    break;
+            }
+
+
+            return true;
+        });
+    }
+
+
+    /** methods for camera */
+    private void EnableRuntimePermission() {
+        if (ActivityCompat.shouldShowRequestPermissionRationale(my_outfits.this, Manifest.permission.CAMERA)) {
+            Toast.makeText(my_outfits.this, "CAMERA permission allows us to Access your camera", Toast.LENGTH_LONG).show();
+        }
+        else
+        {
+            ActivityCompat.requestPermissions(my_outfits.this, new String[]{Manifest.permission.CAMERA}, RequestPermissionCode);
+        }
+    }
+
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+        if (requestCode == 7 && resultCode == RESULT_OK) {
+            Bitmap bitmap = (Bitmap) data.getExtras().get("data");
+            /** Instead of all below code, we will pass bitmap and tag string to method CreateItem(); */
+            //ImageView image = new ImageView(this);/** This code adds a button each time*/
+            //image.setLayoutParams(gridLayout.getLayoutParams());
+            //gridLayout.addView(image);
+            //image.setImageBitmap(bitmap);
+        }
+    }
+
+    public void onRequestPermissionsResult(int requestCode, String permissions[], int[] result) {
+        super.onRequestPermissionsResult(requestCode, permissions, result);
+        switch (requestCode) {
+            case RequestPermissionCode:
+                if (result.length > 0 && result[0] == PackageManager.PERMISSION_GRANTED) {
+                    Toast.makeText(my_outfits.this, "Permission Granted, Now your application can access CAMERA.", Toast.LENGTH_LONG).show();
+                } else {
+                    Toast.makeText(my_outfits.this, "Permission Canceled, Now your application cannot access CAMERA.", Toast.LENGTH_LONG).show();
+                }
+                break;
+        }
     }
 }
