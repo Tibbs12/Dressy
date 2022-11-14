7eaa9ae1624bbda3423078dd257b715e6088d04a
Ruuse <russom4883@uhcl.edu>
2022-11-14 16:44:22 -0600

    quick fix for manifest file

--- app/src/main/java/com/closetkeeper/dressy/home.java
+++ app/src/main/java/com/closetkeeper/dressy/home.java
@@ -59,39 +59,6 @@ public class home extends AppCompatActivity {
         setContentView(binding.getRoot());
 
 
-        //Navigation menu code
-        binding.bottomNavBar.setOnItemSelectedListener(item -> {
-
-            switch(item.getItemId()){
-                case R.id.homeNavBtn:
-                    Intent intent = new Intent(this, com.closetkeeper.dressy.home.class);
-                    startActivity(intent);
-                    break;
-                case R.id.closetNavBtn:
-                    Intent closet = new Intent(this, com.closetkeeper.dressy.my_closets.class);
-                    startActivity(closet);
-                    break;
-                case R.id.addNavBtn:
-                    /**Intent add = new Intent(this, com.closetkeeper.dressy.???.class);
-                     startActivity(add); */
-                    EnableRuntimePermission();
-                    Intent camera = new Intent(android.provider.MediaStore.ACTION_IMAGE_CAPTURE);
-                    startActivityForResult(camera, 7); /** this is connected to "OnActivityResult" Method */
-                    break;
-                case R.id.searchNavBar:
-                    /** Intent search = new Intent(this, com.closetkeeper.dressy.my_closets.class);
-                     startActivity(search); */
-                    break;
-                case R.id.outfitsNavBtn:
-                    Intent outfit = new Intent(this, com.closetkeeper.dressy.my_outfits.class);
-                    startActivity(outfit);
-                    break;
-            }
-
-
-            return true;
-        });
-
         MyClosets = (LinearLayout) findViewById(R.id.MyClosets);
         MyOutfits = (LinearLayout) findViewById(R.id.MyOutfits);
 
@@ -119,6 +86,40 @@ public class home extends AppCompatActivity {
             public void onClick(View v) {
                 openCalendar();
             }
+        });
+
+
+        //Navigation menu code
+        binding.bottomNavBar.setOnItemSelectedListener(item -> {
+
+            switch(item.getItemId()){
+                case R.id.homeNavBtn:
+                    Intent intent = new Intent(this, com.closetkeeper.dressy.home.class);
+                    startActivity(intent);
+                    break;
+                case R.id.closetNavBtn:
+                    Intent mycloset = new Intent(this, com.closetkeeper.dressy.my_closets.class);
+                    startActivity(mycloset);
+                    break;
+                case R.id.addNavBtn:
+                    /**Intent add = new Intent(this, com.closetkeeper.dressy.???.class);
+                     startActivity(add); */
+                    EnableRuntimePermission();
+                    Intent camera = new Intent(android.provider.MediaStore.ACTION_IMAGE_CAPTURE);
+                    startActivityForResult(camera, 7); /** this is connected to "OnActivityResult" Method */
+                    break;
+                case R.id.searchNavBar:
+                    /** Intent search = new Intent(this, com.closetkeeper.dressy.my_closets.class);
+                     startActivity(search); */
+                    break;
+                case R.id.outfitsNavBtn:
+                    Intent outfit = new Intent(this, com.closetkeeper.dressy.my_outfits.class);
+                    startActivity(outfit);
+                    break;
+            }
+
+
+            return true;
         });
 
         /** End of OnClick Listeners */
--- app/src/main/java/com/closetkeeper/dressy/ui/MainActivity.java
+++ app/src/main/java/com/closetkeeper/dressy/ui/MainActivity.java
@@ -3,26 +3,33 @@ package com.closetkeeper.dressy.ui;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
+import android.graphics.Bitmap;
 import android.os.Bundle;
-import android.view.View;
-import android.widget.Button;
+import android.widget.ArrayAdapter;
 
 import com.closetkeeper.dressy.R;
+
+import java.util.ArrayList;
+import java.util.List;
 
 public class MainActivity extends AppCompatActivity {
 
 
+
+
+    /** Need static variables and methods here to keep list of items, closets, and outfits updated */
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-
         //We will first display the contents of the splash screen and then move to access page
 
         Intent intent = new Intent(this, com.closetkeeper.dressy.access.class);
         startActivity(intent);
 
     }
+    /** this is where the static methods to add or remove items from the lists should be */
+
 
 
 
