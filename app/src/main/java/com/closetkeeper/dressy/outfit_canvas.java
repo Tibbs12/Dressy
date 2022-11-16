9e90bce3b2d8bc3ae7cc86173395186332527458
Ruuse <russom4883@uhcl.edu>
2022-11-16 00:26:03 -0600

    done

--- app/src/main/java/com/closetkeeper/dressy/outfit_canvas.java
+++ app/src/main/java/com/closetkeeper/dressy/outfit_canvas.java
@@ -2,13 +2,51 @@ package com.closetkeeper.dressy;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
 
+import com.closetkeeper.dressy.databinding.ActivityOutfitCanvasBinding;
+
 public class outfit_canvas extends AppCompatActivity {
+
+    ActivityOutfitCanvasBinding binding;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_outfit_canvas);
+        binding = ActivityOutfitCanvasBinding.inflate(getLayoutInflater());
+        setContentView(binding.getRoot());
+
+        //Navigation menu code
+        binding.bottomNavBar.setOnItemSelectedListener(item -> {
+
+            switch(item.getItemId()){
+                case R.id.homeNavBtn:
+                    Intent intent = new Intent(this, com.closetkeeper.dressy.home.class);
+                    startActivity(intent);
+                    break;
+                case R.id.closetNavBtn:
+                    Intent mycloset = new Intent(this, com.closetkeeper.dressy.my_closets.class);
+                    startActivity(mycloset);
+                    break;
+                case R.id.addNavBtn:
+                    /**Intent add = new Intent(this, com.closetkeeper.dressy.???.class);
+                     startActivity(add); */
+                    Intent camera = new Intent(android.provider.MediaStore.ACTION_IMAGE_CAPTURE);
+                    startActivityForResult(camera, 7); /** this is connected to "OnActivityResult" Method */
+                    break;
+                case R.id.searchNavBar:
+                    /** Intent search = new Intent(this, com.closetkeeper.dressy.my_closets.class);
+                     startActivity(search); */
+                    break;
+                case R.id.outfitsNavBtn:
+                    Intent outfit = new Intent(this, com.closetkeeper.dressy.my_outfits.class);
+                    startActivity(outfit);
+                    break;
+            }
+
+
+            return true;
+        });
     }
 }
